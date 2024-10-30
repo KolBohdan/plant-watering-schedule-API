af2b786e9e878d5b54245709de98be5ff704930e
Bohdan Kolomiiets <kb907406@gmail.com>
2024-10-30 18:34:13 +0200

    Add Plant model

--- garden/models.py
+++ garden/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    watering_frequency_days = models.PositiveIntegerField()
+    last_watered_date = models.DateField()
+
+    class Meta:
+        ordering = ["-last_watered_date"]
+
+    def __str__(self) -> str:
+        return self.name
