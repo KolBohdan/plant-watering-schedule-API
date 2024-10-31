1f4112498c2c3a12c3027e48be49aa21bad524bb
Bohdan Kolomiiets <kb907406@gmail.com>
2024-10-31 11:48:30 +0200

    Implement PlantMarkAsWateredSerializer

--- garden/serializers.py
+++ garden/serializers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework import serializers
 
 from garden.models import Plant
@@ -21,3 +23,22 @@ class PlantUpdateSerializer(serializers.ModelSerializer):
         model = Plant
         fields = ("last_watered_date",)
 
+
+class PlantMarkAsWateredSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plant
+        fields = (
+            "id",
+            "name",
+            "species",
+            "watering_frequency_days",
+            "last_watered_date",
+            "is_watered"
+        )
+        read_only_fields = fields
+
+    def update(self, instance, validated_data):
+        instance.last_watered_date = datetime.date.today()
+        instance.save()
+
+        return instance
