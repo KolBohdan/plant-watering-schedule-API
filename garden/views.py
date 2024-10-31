0252ec9fa2075bc4f2bd1d77cfa2950c91defb26
Bohdan Kolomiiets <kb907406@gmail.com>
2024-10-31 11:48:58 +0200

    Add custom action mars_as_watered

--- garden/views.py
+++ garden/views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets, mixins
+import datetime
 
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from garden.models import Plant
-from garden.serializers import PlantSerializer, PlantUpdateSerializer
+from garden.serializers import PlantSerializer, PlantUpdateSerializer, PlantMarkAsWateredSerializer
 
 
 class PlantViewSet(
@@ -17,5 +20,22 @@ class PlantViewSet(
     def get_serializer_class(self):
         if self.action == "update":
             return PlantUpdateSerializer
+        elif self.action == "mark_as_watered":
+            return PlantMarkAsWateredSerializer
 
         return PlantSerializer
+
+    @action(
+        methods=["PUT"],
+        detail=True,
+        url_path="mark",
+    )
+    def mark_as_watered(self, request, pk=None):
+        """Endpoint for marking specific plant as watered"""
+        plant = self.get_object()
+        serializer = self.get_serializer(plant, data=request.data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
